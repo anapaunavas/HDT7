2a0e12dc36346fd10e7ac0ca3c790c5d580e45c4
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2023-03-25 15:15:52 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
-public class Main {}
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+    private static BinarySearchTree<String, String> englishSpanishDictionary = new BinarySearchTree<String,String>(new compareStrings()<String>());
+    private static BinarySearchTree<String, String> frenchSpanishDictionary = new BinarySearchTree<String,String>(new compareStrings()<String>());
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+		System.out.println("Bienvenido al diccionario traductor :) ");
+		start();
+    }
+}
