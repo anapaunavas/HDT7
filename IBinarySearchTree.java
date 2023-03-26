1d7a83510b0a8e7bb6881d186d05112ee117c2a6
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2023-03-25 19:39:57 -0500

    Update IBinarySearchTree.java

--- IBinarySearchTree.java
+++ IBinarySearchTree.java
@@ -1,5 +1,22 @@
 import java.util.ArrayList;
 
-public interface IBinarySearchTree<T1, T2> {
-
+public interface IBinarySearchTree<K, V> {
+    void insert(K id, V value);
+	
+	V delete(K id);
+	
+	V find(K id);
+	
+	int count();
+	
+	boolean isEmpty();
+	
+	ArrayList<V> getElements();
+	
+	void inOrder(ITreeTraversal<V> traversal);
+	
+	void preOrder(ITreeTraversal<V> traversal);
+	
+	void postOrder(ITreeTraversal<V> traversal);
+	
 }
